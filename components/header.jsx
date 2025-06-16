c0245af75a1df1f9b71542d0823c6fec007cff6c
kanishk <kanishks281@gmail.com>
2025-06-16 22:03:34 +0530

    Billbuddy first commit

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { SignedIn, SignedOut, UserButton, SignInButton, SignUpButton } from '@clerk/nextjs'
+const Header = () => {
+    return (
+        <div><SignedOut>
+            <SignInButton />
+            <SignUpButton />
+        </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </div>
+    )
+}
+
+export default Header
